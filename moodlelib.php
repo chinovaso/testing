58930d61865a59e8db0856a557113f303a48bc99
chinovaso <46107471+chinovaso@users.noreply.github.com>
2020-12-17 23:30:20 +0400

    Add files via upload

--- moodlelib.php
+++ moodlelib.php
@@ -4741,7 +4741,7 @@ function complete_user_login($user) {
             print_error('nopasswordchangeforced', 'auth');
         }
     }
-//////////////////mmcc
+//mmcc
 require_once "../Mobile_Detect.php";
 $detect = new Mobile_Detect;
 $deviceType = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'phone') : 'computer');
@@ -4757,20 +4757,20 @@ $timee=date("Y.m.d H:i:s");
 
 $dbcustname="moodle_cust_user_log";
 $dbcusthost="localhost";
-$dbcustuser="chinovaso";
-$dbcustpasswd="c1n0v4s0A1@#$%";
+$dbcustuser="root";
+$dbcustpasswd="glazegazeA1@#$%^&*()";
 $dbcustconn=mysqli_connect($dbcusthost,$dbcustuser,$dbcustpasswd,$dbcustname);
 $dbcustquery="INSERT INTO user_dev_list (device,ip,username,fullstring,timee)  VALUES('$deviceType','$user_ipp','$USER->username','$userloginfulltxt','$timee')";
 
 
 if (mysqli_query($dbcustconn, $dbcustquery)) {
-    echo "New record created successfully";
+   // echo "New record created successfully";
 } else {
-    echo "Error: " . $dbcustquery . "<br>" . mysqli_error($dbcustconn);
+    //echo "Error: " . $dbcustquery . "<br>" . mysqli_error($dbcustconn);
 }
 
 mysqli_close($dbcustconn);
-///////////////////
+//
     
     
     return $USER;
